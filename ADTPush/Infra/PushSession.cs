355d3d79b092a34d4b24c753dc3ae17713c72ccb
dejung <dejung@adt.co.kr>
2018-05-18 18:26:18 +0900

    bbbb

--- ADTPush/Infra/PushSession.cs
+++ ADTPush/Infra/PushSession.cs
@@ -9,10 +9,8 @@ namespace ADTPush.Infra
 {
     public class PushSession : AppSession<PushSession, PushRequestInfo>
     {
-        public Context context;
         protected override void OnSessionStarted()
         {
-            context = new Context(this);
         }
 
         protected override void OnSessionClosed(CloseReason reason)
